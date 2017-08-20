dd8b5d3ade3f9bd5193f9d500a6419239351e81e
nikhilsteph <nikil.steph@gmail.com>
2017-08-20 19:11:37 +0530

    added robocomps generated components

---
+++ testrobocompdsl/gen_py/src/genericworker.py
@@ -0,0 +1,178 @@
+#
+# Copyright (C) 2017 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys, Ice, os
+from Queue import Queue, Empty
+from PySide import *
+
+ROBOCOMP = ''
+try:
+	ROBOCOMP = os.environ['ROBOCOMP']
+except KeyError:
+	print '$ROBOCOMP environment variable not set, using the default value /opt/robocomp'
+	ROBOCOMP = '/opt/robocomp'
+
+preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ --all /opt/robocomp/interfaces/"
+Ice.loadSlice(preStr+"CommonBehavior.ice")
+import RoboCompCommonBehavior
+
+additionalPathStr = ''
+icePaths = []
+try:
+	icePaths.append('/opt/robocomp/interfaces')
+	SLICE_PATH = os.environ['SLICE_PATH'].split(':')
+	for p in SLICE_PATH:
+		icePaths.append(p)
+		additionalPathStr += ' -I' + p + ' '
+except:
+	print 'SLICE_PATH environment variable was not exported. Using only the default paths'
+	pass
+
+ice_Test = False
+for p in icePaths:
+	print 'Trying', p, 'to load Test.ice'
+	if os.path.isfile(p+'/Test.ice'):
+		print 'Using', p, 'to load Test.ice'
+		preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ " + additionalPathStr + " --all "+p+'/'
+		wholeStr = preStr+"Test.ice"
+		Ice.loadSlice(wholeStr)
+		ice_Test = True
+		break
+if not ice_Test:
+	print 'Couln\'t load Test'
+	sys.exit(-1)
+from RoboCompTests import *
+
+
+from outtestI import *
+
+
+class GenericWorker(QtCore.QObject):
+	kill = QtCore.Signal()
+
+
+	def __init__(self, mprx):
+		super(GenericWorker, self).__init__()
+
+
+
+
+		self.mutex = QtCore.QMutex(QtCore.QMutex.Recursive)
+		self.Period = 30
+		self.timer = QtCore.QTimer(self)
+
+
+	@QtCore.Slot()
+	def killYourSelf(self):
+		rDebug("Killing myself")
+		self.kill.emit()
+
+	# \brief Change compute period
+	# @param per Period in ms
+	@QtCore.Slot(int)
+	def setPeriod(self, p):
+		print "Period changed", p
+		Period = p
+		timer.start(Period)
+
+
+	#
+	# substract
+	#
+	def substract(self, num1,num2):
+		kwargs = {"num1":num1,"num2":num2,}
+		cid = self.substractBuffer.push(kwargs)
+		while(self.substractBuffer.is_finished(cid)==False): pass
+		return tuple(self.substractBuffer.result(cid))
+
+	#
+	# printmsg
+	#
+	def printmsg(self, message):
+		kwargs = {"message":message,}
+		cid = self.printmsgBuffer.push(kwargs)
+
+	#
+	# sum
+	#
+	def sum(self, num1,num2):
+		kwargs = {"num1":num1,"num2":num2,}
+		cid = self.sumBuffer.push(kwargs)
+		while(self.sumBuffer.is_finished(cid)==False): pass
+		return tuple(self.sumBuffer.result(cid))
+
+	#
+	# divide
+	#
+	def divide(self, divident,divisor):
+		kwargs = {"divident":divident,"divisor":divisor,}
+		cid = self.divideBuffer.push(kwargs)
+		while(self.divideBuffer.is_finished(cid)==False): pass
+		return tuple(self.divideBuffer.result(cid))
+
+
+
+class CallQueue(object):
+	def __init__(self, maxsize=-1):
+		self.mutex = QtCore.QMutex()
+		self.call_buffer = Queue(maxsize=maxsize)
+		self.current_id = 0
+		self.result_buffer = dict()
+
+	def push(self, params):
+		"""push a call to queue"""
+		self.mutex.lock()
+		self.call_buffer.put_nowait((self.current_id, params))
+		cid = self.current_id
+		self.current_id += 1
+		self.mutex.unlock()
+		return cid
+
+	def pop(self):
+		"""pop a call from the queue"""
+		try:
+			self.mutex.lock()
+			cid, params = self.call_buffer.get_nowait()
+			self.mutex.unlock()
+			return params, cid
+		except Empty:
+			self.mutex.unlock()
+			return None, None
+
+	def result(self, cid):
+		""" return result of an call """
+		if self.is_finished(cid):
+			result = self.result_buffer[cid]
+			del self.result_buffer[cid]
+			return result
+		else:
+			raise Exception("Id not Found:"+str(cid))
+
+	def is_finished(self, cid):
+		""" if a call has finished """
+		return cid in self.result_buffer.keys()
+
+	def set_finished(self, cid, result=None):
+		""" set a call as finished"""
+		if not result:
+			pass
+		self.result_buffer[cid] = result
+
+	def empty(self):
+		""" """
+		return self.call_buffer.empty()
---
+++ testrobocompdsl/gen_py/src/specificworker.py
@@ -0,0 +1,120 @@
+#
+# Copyright (C) 2017 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os, traceback, time
+
+from PySide import *
+from genericworker import *
+
+# If RoboComp was compiled with Python bindings you can use InnerModel in Python
+# sys.path.append('/opt/robocomp/lib')
+# import librobocomp_qmat
+# import librobocomp_osgviewer
+# import librobocomp_innermodel
+
+class SpecificWorker(GenericWorker):
+	def __init__(self, proxy_map):
+		super(SpecificWorker, self).__init__(proxy_map)
+		self.timer.timeout.connect(self.compute)
+		self.Period = 2000
+		self.timer.start(self.Period)
+
+		self.substractBuffer = CallQueue()
+		self.printmsgBuffer = CallQueue()
+		self.sumBuffer = CallQueue()
+		self.divideBuffer = CallQueue()
+
+
+	def setParams(self, params):
+		#try:
+		#	self.innermodel = InnerModel(params["InnerModelPath"])
+		#except:
+		#	traceback.print_exc()
+		#	print "Error reading config params"
+		return True
+
+	@QtCore.Slot()
+	def compute(self):
+		print 'SpecificWorker.compute...'
+		#computeCODE
+		#try:
+		#	self.differentialrobot_proxy.setSpeedBase(100, 0)
+		#except Ice.Exception, e:
+		#	traceback.print_exc()
+		#	print e
+
+		# The API of python-innermodel is not exactly the same as the C++ version
+		# self.innermodel.updateTransformValues("head_rot_tilt_pose", 0, 0, 0, 1.3, 0, 0)
+		# z = librobocomp_qmat.QVec(3,0)
+		# r = self.innermodel.transform("rgbd", z, "laser")
+		# r.printvector("d")
+		# print r[0], r[1], r[2]
+
+		# You can call the methods on proxy in blocking or non blocking way (async)
+		# 
+		# for calling in blocking way 
+		# a, b = self.test_proxy.sum(100, 2)
+		# 
+		# async call using polling
+		# 	call1 = test_proxy.begin_sum(100, 2)
+		# poll using
+		# 	call1.isCompleted()
+		# get the return value
+		# 	a,b = test_proxy.end_sum(call1)
+		# 
+		# async call uing callback
+		# 	test_proxy.begin_sum(100, 2, lambda x:print(x), lambda ex:print(ex)
+		# 
+
+
+		# substract
+		params, cid = self.substractBuffer.pop()
+		if cid is not None:
+			num1 = params["num1"]
+			num2 = params["num2"]
+			result = num1 - num2
+			self.substractBuffer.set_finished(cid, [result] )
+
+
+		# printmsg
+		params, cid = self.printmsgBuffer.pop()
+		if cid is not None:
+			message = params["message"]
+			print(message)
+
+		# sum
+		params, cid = self.sumBuffer.pop()
+		if cid is not None:
+			num1 = params["num1"]
+			num2 = params["num2"]
+			ret = num2+num1
+			self.sumBuffer.set_finished(cid, [ret])
+
+
+		# divide
+		params, cid = self.divideBuffer.pop()
+		if cid is not None:
+			divident = params["divident"]
+			divisor = params["divisor"]
+			ret = divident/divisor
+			reminder = divident%divisor
+			self.divideBuffer.set_finished(cid, [ret, reminder])
+
+		return True
+
